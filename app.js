b0a90bef4d38e02f40729022941047069a3bca5e
Nick Malakar <nickmalakar@gmail.com>
2017-09-08 15:35:33 -0400

    starter models, routes, public folders, app.js and schema.sql added

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 let express = require('express');
 
 // Route files
-
+let index = require('./routes/index');
+let login = require('./routes/login');
+let logout = require('./routes/logout');
+let signup = require('./routes/signup');
+let group = require('./routes/group');
+let user = require('./routes/user');
+let invite = require('routes/invite');
 
 // Models
 let db = require('./models');
@@ -26,6 +32,13 @@ let app = express();
 
 
 // Routing
+app.use('/', index);
+app.use('/login', login);
+app.use('/logout', logout);
+app.use('/signup', signup);
+app.use('/group/:id', group);
+app.use('/user/:id', user);
+app.use('/invite/group/:id/user/:id', invite);
 
 
 // Catch 404 and forward to error handler
