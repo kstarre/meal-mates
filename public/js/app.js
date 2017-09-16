d4ace15cf7ace57fdcfd764b67909dada7018f5f
MeganDarwin <MeganDarwin@gmail.com>
2017-09-16 11:11:30 -0400

    working on full calendar

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,31 @@ $("#register-button").on("submit",function(e) {
    window.location.replace("./editprofile.html");
 });
 
-  
+//if user is logged then navbar will display
 
+//if member is not in a group then group.html will ask whether they want to create one
+
+//if member creates a group they will have their admin boolean change from default false to true
+
+
+//if member is and admin of a group the group page will have editing capability
+
+// rendering sql data on profile page at index
+
+
+// rendering group data on group profile page at index
+
+// full calendar js
+$('#calendar').fullCalendar({
+        // put your options and callbacks here
+
+header: {
+    left:   'title',
+    center: '',
+    right:  'today prev,next'
+},
+
+    })
 });
 
 
