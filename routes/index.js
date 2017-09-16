11a29ee963822e4d450da0856326c9243f6f7dd5
Nick Malakar <nickmalakar@gmail.com>
2017-09-16 13:51:01 -0400

    group controller file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-let indexController = require("../controllers/indexController");
+//let indexController = require("../controllers/indexController");
 let passport = require('passport');
 
 
@@ -9,22 +9,19 @@ router.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-router.get("/signup", indexController.signup);
-
-router.get("/signin", indexController.signin);
-
 router.get("/signout", indexController.signout);
 
 
 // Post routes
 router.post("/signup", passport.authenticate('local-signup', {
-	successRedirect: '/',
+	successRedirect: '/editprofile/:id',
 	failureRedirect: '/signup',
 	failureFlash: true
-}));
+ }));
+
 
 router.post('/signin', passport.authenticate('local-signin', {
-	successRedirect: '/',
+	successRedirect: '/editprofile/:id',
 	failureRedirect: '/signin',
 	failureFlash: true
 }));
