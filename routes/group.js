6dd50db8dd274ce8fb2edd36df4d82154fd87067
Katie Starrett <starrett.k.e@gmail.com>
2017-09-21 18:03:42 -0400

    group routes

--- routes/group.js
+++ routes/group.js
@@ -1,17 +1,27 @@
 let groupController = require("../controllers/groupController");
+let indexController = require("../controllers/indexController");
 
 module.exports = function(app) {
+	// HTML Routes
+	app.get("/group", indexController.isLoggedIn, groupController.groupView),
+	app.get("/group/create", indexController.isLoggedIn, groupController.createGroupView),
+	app.get("/group/admin", indexController.isLoggedIn, groupController.adminGroupView),
+	app.get("/group/calendar", indexController.isLoggedIn, groupController.viewCalendar),
+	app.get("/group/calendar/admin", indexController.isLoggedIn, groupController.adminCalendarView),
+
+
+	// API Routes
 	// get group page
-	app.get("/group", groupController.group),
+	app.get("/api/group", groupController.getGroup),
 
 	// create new group page
-	app.get("/group/new", groupController.groupNew),
+	app.get("/api/group/new", groupController.groupNewGroup),
 
 	// edit group page
-	app.get("/group/edit", groupController.groupEdit),
+	app.get("/api/group/edit", groupController.groupEdit),
 
 	// delete group page
-	app.get("/group/edit", groupController.groupDelete),
+	app.get("/api/group/delete", groupController.groupDelete),
 
 	// calendar route
 	app.get("/group/calendar", groupController.calendar)
