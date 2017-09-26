da7d686f3ed8a701e66c3c99f1cf31e17bbb146d
Katie Starrett <starrett.k.e@gmail.com>
2017-09-26 19:37:34 -0400

    fixing branch

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const db = require("../models");
 
-
 module.exports = {
 	// HTML Routes
 	createGroupView: function(req, res) {
@@ -20,30 +19,9 @@ module.exports = {
 		res.sendFile(path.join(__dirname, "../public/admincalendar.html"));
 	},
 
-
-	groupView: function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/group.html"));
-	},
-	groupAdmin: function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/group.html"));
-	},
-	groupCalendar: function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/calendar.html"));
-	},
-	groupAdminCal: function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/calendar.html"));
-	},
-  
-	// GET group
-	group: function(req, res) {
-		console.log("\nreq.params.id");
-		console.log(req.params.id);
-		
-},
 	// API Routes
 	// GET group
 	getGroup: function(req, res) {  
-
 		db.Lunchgroup.findOne({
 	  		where: {
 	  			id: req.params.id
@@ -93,36 +71,12 @@ module.exports = {
     	});		
 	},
 
-	// GET calendar route
-	// calendar: function(req, res) {
-	// 	res.sendFile(path.join(__dirname, "../public/calendar.html"));
-	// }
-
-
-    // calendar route
-    // calendar = function() {
-
 	getCalendarInfo: function(req, res) {
 		// not complete
-		db.Eventdate.findOne({
-			where: {
-				groupName: req.body.name
-			}, 
-			include: {
-				model: db.Lunchgroup,
-				model: db.User
-			}
-		}).then(function(calendar) {
-		res.json(calendar);
-	  	// if group does not exist
-		}).catch(function(err) {
-			res.redirect("/");
-		});
 	},
 
 	calendarEdit: function(req, res) {
 		// Not complete
 	}
 
-    // }
-}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 let indexController = require("../controllers/indexController");
 console.log("indexController!");
-console.log(indexController);
+
 module.exports = function(app, passport) {
 	// HTML routes
 	app.get("/", indexController.home),
