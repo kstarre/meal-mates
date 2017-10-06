660affdc5d79efcb1889f904f215473bc3b1eac3
Gregory T Draheim <GregoryTDraheim@gmail.com>
2017-10-05 21:29:13 -0400

    image uploader uploading, needs displaying

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,85 +2,80 @@ const path = require("path");
 const db = require("../models");
 
 module.exports = {
-    // HTML Routes
-    home: function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    },
-    viewProfile: function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/viewprofile.html"));
-    },
-    editProfile: function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/editprofile.html"));
-    },
-    welcome: function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/welcome.html"));
-    },
+	// HTML Routes
+	home: function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/index.html"));
+	},
+	viewProfile: function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/viewprofile.html"));
+	},
+	editProfile: function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/editprofile.html"));
+	},
+	welcome: function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/welcome.html"));
+	},
 
-    // API Routes
-    isLoggedIn: function(req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.redirect("/");
-    },
-    isAdmin: function(req, res, next) {
-        if (req.user.admin) {
-            return next();
-        }
-        res.redirect("/");
-    },
-    logout: function(req, res) {
-        req.session.destroy(function(err) {
-            res.redirect("/");
-        })
-    },
-    getUserInfo: function(req, res) {
-        db.User.findOne({
-            where: {
-                id: req.user.id
-            },
-            include: [{
-                model: db.Lunchgroup,
-                where: { id: req.user.LunchgroupId }
-            }]
-        }).then(results => {
-        	console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        	console.log(results);
-            res.json(results);
-        });
-    },
-    updateUserInfo: function(req, res) {
-        if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
-            db.User.update({
-                LunchgroupId: null
-            }, {
-                where: {
-                    id: req.user.id
-                }
-            }).then(function(results) {
-                res.json(results);
-            })
-        } else {
-            db.User.update(req.body, {
-                where: {
-                    id: req.user.id
-                }
-            }).then(function(results) {
-                res.json(results);
-            });
-        }
-    },
-    deleteUser: function(req, res) {
-        db.User.destroy({
-            where: {
-                id: req.user.id
-            }
-        }).then(function(results) {
-            res.json(results);
-        });
-    },
+	// API Routes
+	isLoggedIn: function(req, res, next) {
+		if ( req.isAuthenticated() ) {
+			return next();
+		}
+		res.redirect("/");
+	},
+	isAdmin: function(req, res, next) {
+		if (req.user.admin) {
+			return next();
+		}
+		res.redirect("/");
+	},
+	logout: function(req, res) {
+		req.session.destroy(function(err) {
+			res.redirect("/");
+		})
+	},
+	getUserInfo: function(req, res) {
+		db.User.findOne({
+			where: {
+				id: req.user.id
+			}
+		}).then( results => {
+			res.json(results);
+		});
+	},
+	updateUserInfo: function(req, res) {
+		if (Object.keys(req.body).length === 0 && req.body.constructor === Object) {
+			db.User.update({
+				LunchgroupId: null
+			}, {
+				where: {
+					id: req.user.id
+				}
+			}).then(function(results) {
+				res.json(results);
+			})
+		}
+		else {
+			db.User.update(req.body, {
+				where: {
+					id: req.user.id
+				}
+			}).then(function(results) {
+				res.json(results);
+			});
+		}
+	},
+	deleteUser: function(req, res) {
+		db.User.destroy({
+			where: {
+				id: req.user.id
+			}
+		}).then(function(results) {
+			res.json(results);
+		});
+	},
 
-    uploadImage: function(req, res) {
+	 uploadImage: function (req, res) {
             message = '';
             if (req.method == "POST") {
                 var post = req.body;
@@ -134,17 +129,5 @@ else {
     console.log("ELSE")
 }
 
-},
-
-// getImage function(req, res) {
-//     var message = '';
-//     var id = req.params.id;
-//     var sql = "SELECT * FROM `users_image` WHERE `id`='" + id + "'";
-//     db.query(sql, function(err, result) {
-//         if (result.length <= 0)
-//             message = "Profile not found!";
-
-//         res.render('profile.ejs', { data: result, message: message });
-//     });
-// };
+}
 };
