616ee9ade6eee569c890dce576b5a30026ca2111
Gregory T Draheim <GregoryTDraheim@gmail.com>
2017-10-07 09:53:58 -0400

    image saving stuff

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -85,5 +85,62 @@ module.exports = {
 		}).then( results => {
 			res.json(results);
 		});
-	}
+	},
+	
+	 uploadImage: function(req, res) {
+	    	
+            message = '';
+            if (req.method == "POST") {
+                var post = req.body;
+                var name = post.user_name;
+                var pass = post.password;
+                var fname = post.first_name;
+                var lname = post.last_name;
+                var mob = post.mob_no;
+
+                if (!req.files)
+                    return res.status(400).send('No files were uploaded.');
+
+                var file = req.files.uploaded_image;
+                var img_name = file.name;
+
+                console.log(file.mimetype);
+
+                if (file.mimetype == "image/jpeg" || file.mimetype == "image/png" || file.mimetype == "image/gif") {
+                	console.log(__dirname + '/../public/img/images/upload_images/');
+
+                    file.mv(__dirname + '/../public/img/images/upload_images/' + file.name, function(err) {
+
+                        if (err) 
+
+                            return res.status(500).send(err);
+                        
+                        db.User.update({
+                            imageLink: img_name
+
+                        }, {
+                            where: {
+                                id: req.user.id
+                            }
+                        }).then(function(results) {
+
+                            res.redirect("/editprofile");
+                        })
+
+                    });
+                
+
+        } else {
+            message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+            // res.render('index.ejs', { message: message });
+            console.log("message");
+            console.log(message);
+        }
+}
+else {
+    // res.render('index');
+    console.log("ELSE")
+}
+
+},
 };
