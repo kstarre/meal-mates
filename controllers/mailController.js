8f41ff33014f3a531fc467471c88906d34dc641c
Katie Starrett <starrett.k.e@gmail.com>
2017-10-17 16:55:50 -0400

    move code from mail route to mail controller

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,5 +1,32 @@
-const path = require("path");
+let nodemailer = require('nodemailer');
+require('dotenv').config();
 
 module.exports = {
+	send: function(req, res) {
+		var smtpTransport = nodemailer.createTransport({
+		    service: "hotmail",
+		    // host: "smtp.gmail.com",
+		    auth: {
+		        user: "mealmates@hotmail.com",
+		        pass: process.env.HPASS
+		    }
+		});
 
+		//code to send e-mail.
+		var mailOptions = {
+		    to: req.query.to,
+		    subject: req.query.subject,
+		    text: req.query.text
+		};
+		//console.log(mailOptions);
+		smtpTransport.sendMail(mailOptions, function(error, response) {
+		    if (error) {
+		        console.log(error);
+		        res.end("error");
+		    } else {
+		        console.log("Message sent: " + response.message);
+		        res.end("sent");
+		    }
+		});
+	}
 };
